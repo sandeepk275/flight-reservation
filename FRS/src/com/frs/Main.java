521d9748393904eca05505ffe6b6b6078ae756f8
sandeepk275 <sandeep.kumar41991@gmail.com>
2021-09-09 22:01:25 +0530

    checkpoint 5

--- FRS/src/com/frs/Main.java
+++ FRS/src/com/frs/Main.java
@@ -8,7 +8,7 @@ public class Main {
         Passenger newPassenger = new Passenger("kholi", "bali", "puducheri", "kamla", "124421",
                 "ktch@gmail.com");
         Ticket ticket1 = new Ticket("1234", "mumbai", "goa", null, "12:30",
-                "1:20", null, "q123", 3025, false);
+                "1:20", newPassenger, "q123", 3025, false);
         Flight flight1 = new Flight("q231", "indigo", 145, 120);
 
         System.out.println(flight1.getFlightDetails());
